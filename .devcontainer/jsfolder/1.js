76b2008bf2cb8b6ccbbc5c08d5b3623e843dd342
uppu123 <uk8530@gmail.com>
2025-06-05 12:28:20 +0000

    js

--- .devcontainer/jsfolder/1.js
+++ .devcontainer/jsfolder/1.js
@@ -1,1 +1,27 @@
-console.log("Hello WOrld");
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+
+// let str1 = "Hello"
+// let str2 = " World"
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+
+// console.log("2" + 1)
+// console.log(2 + "1")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+
+
+// console.log(true)
+// console.log(+true)
+
+
+
+console.log("1" > 2)
+console.log("2" == 2)
